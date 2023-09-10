ef728d0314ad305c6a1c68e2653845c112c01aca
PabloG55 <pasebarona@gmail.com>
2023-09-10 14:09:03 -0400

    Update Project_pablo_garces.java
    Added code for demo class.

--- Project_pablo_garces.java
+++ Project_pablo_garces.java
@@ -2,51 +2,41 @@ import java.util.Scanner; // for reading user input
 
 public class Project_pablo_garces
 {
+    private static Scanner scnr = new Scanner(System.in); // Initialize the scanner object
     public static void main (String[]args)
     {
+        //Declare all the variables.
+        int policyNumber, policyholderAge;
+        double policyholderHeight, policyholderWeight;
+        String providerName, policyholderFirstname, policyholderLastname, smokingStatus;
 
-        //Create a Scanner object used to read input from the console.
-        Scanner scnr = new Scanner(System.in);
-
-        //Create an instance of the Policy class.
-        Policy policy = new Policy();
-
+        //Get all the information from the user.
         System.out.print("Please enter the Policy Number: ");
-        policy.setPolicyNumber(scnr.nextInt());
+        policyNumber = scnr.nextInt();
         scnr.nextLine();
         System.out.print("Please enter the Provider Name: ");
-        policy.setProviderName(scnr.nextLine());
+        providerName = scnr.nextLine();
         System.out.print("Please enter the Policyholder's First Name: ");
-        policy.setPolicyholderFirstname(scnr.nextLine());
+        policyholderFirstname = scnr.nextLine();
         System.out.print("Please enter the Policyholder's Last Name: ");
-        policy.setPolicyholderLastname(scnr.nextLine());
+        policyholderLastname = scnr.nextLine();
         System.out.print("Please enter the Policyholder's Age: ");
-        policy.setPolicyholderAge(scnr.nextInt());
+        policyholderAge = scnr.nextInt();
         scnr.nextLine();
         System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        policy.setSmokingStatus(scnr.nextLine());
-        while (!(policy.getSmokingStatus().equalsIgnoreCase("smoker") || policy.getSmokingStatus().equalsIgnoreCase("non-smoker")))
-        {
-            System.out.print("Please enter smoker or non-smoker: ");
-            policy.setSmokingStatus(scnr.nextLine());
-        }
-
+        smokingStatus = scnr.nextLine();
+        validateSmokingStatus(smokingStatus);
         System.out.print("Please enter the Policyholder's Height (in inches): ");
-        policy.setPolicyholderHeight(scnr.nextDouble());
-        while (policy.getPolicyholderHeight() <= 0)
-        {
-            System.out.print("Please enter a valid number: ");
-            policy.setPolicyholderHeight(scnr.nextDouble());
-        }
+        policyholderHeight = scnr.nextDouble();
+        validatePolicyholderHeight(policyholderHeight);
         System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-        policy.setPolicyholderWeight(scnr.nextDouble());
-        while (policy.getPolicyholderWeight() <= 0)
-        {
-            System.out.print("Please enter a valid number: ");
-            policy.setPolicyholderWeight(scnr.nextDouble());
-        }
+        policyholderWeight = scnr.nextDouble();
+        validatePolicyholderWeight(policyholderWeight);    
 
+        //Create an instance of the Policy class.
+        Policy policy = new Policy(policyNumber, policyholderAge, policyholderHeight, policyholderWeight, providerName, policyholderFirstname, policyholderLastname, smokingStatus);
 
+        //Print all the information.
         System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
         System.out.println("Provider Name: " + policy.getProviderName());
         System.out.println("Policyholder's First Name: " + policy.getPolicyholderFirstname());
@@ -54,9 +44,50 @@ public class Project_pablo_garces
         System.out.println("Policyholder's Age: " + policy.getPolicyholderAge());
         System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
         System.out.printf("Policyholder's Height: %.1f inches\n" , policy.getPolicyholderHeight());
-        System.out.printf("Policyholder's Weight: %.1f pounds\n" , policy.getPolicyholderWeight());
+        System.out.printf("cPolicyholder's Weight: %.1f pounds\n" , policy.getPolicyholderWeight());
         System.out.printf("Policyholder's BMI: %.2f\n" , policy.bmiCalcualtor());
-        System.out.printf("Policy Price: $%.2f\n" , policy.policyPriceCalculator());
+        System.out.printf("Policy Price: $%.2f\n" , policy.policyPriceCalculator());    
+        
     }
 
+    
+
+    /**
+     * Validate the smoking status from the user.
+     * @param smokingStatus
+     */
+    public static void validateSmokingStatus(String smokingStatus)
+    {
+         while (!(smokingStatus.equalsIgnoreCase("smoker") || smokingStatus.equalsIgnoreCase("non-smoker")))
+        {
+            System.out.print("Please enter smoker or non-smoker: ");
+            smokingStatus = scnr.nextLine();
+        }
+    }
+
+    /**
+     * Validate the policy holder height from the user.
+     * @param policyholderHeight
+     */
+    public static void validatePolicyholderHeight(double policyholderHeight)
+    {
+        while (policyholderHeight <= 0)
+        {
+            System.out.print("Please enter a valid number: ");
+            policyholderHeight = scnr.nextDouble();
+        }
+    }
+
+    /**
+     * Validate the policy holder height from the user.
+     * @param policyholderWeight
+     */
+    public static void validatePolicyholderWeight(double policyholderWeight)
+    {
+        while (policyholderWeight <= 0)
+        {
+            System.out.print("Please enter a valid number: ");
+            policyholderWeight = scnr.nextDouble();
+        }
+    }
 }
