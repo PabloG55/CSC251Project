95906ab68a6fa78fcc86311478edce7cefe0cfdf
PabloG55 <pasebarona@gmail.com>
2023-09-30 00:36:42 -0400

    Update Project_pablo_garces.java

--- Project_pablo_garces.java
+++ Project_pablo_garces.java
@@ -14,39 +14,41 @@ public class Project_pablo_garces
         System.out.print("Please enter the Policy Number: ");
         policyNumber = scnr.nextInt();
         scnr.nextLine();
+
         System.out.print("Please enter the Provider Name: ");
         providerName = scnr.nextLine();
+
         System.out.print("Please enter the Policyholder's First Name: ");
         policyholderFirstname = scnr.nextLine();
+
         System.out.print("Please enter the Policyholder's Last Name: ");
         policyholderLastname = scnr.nextLine();
+
         System.out.print("Please enter the Policyholder's Age: ");
         policyholderAge = scnr.nextInt();
         scnr.nextLine();
+
         System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
         smokingStatus = scnr.nextLine();
         validateSmokingStatus(smokingStatus);
+
         System.out.print("Please enter the Policyholder's Height (in inches): ");
         policyholderHeight = scnr.nextDouble();
         validatePolicyholderHeight(policyholderHeight);
+
         System.out.print("Please enter the Policyholder's Weight (in pounds): ");
         policyholderWeight = scnr.nextDouble();
         validatePolicyholderWeight(policyholderWeight);    
 
         //Create an instance of the Policy class.
-        Policy policy = new Policy(policyNumber, policyholderAge, policyholderHeight, policyholderWeight, providerName, policyholderFirstname, policyholderLastname, smokingStatus);
+        Policy policy = new Policy(policyNumber, policyholderAge, policyholderHeight, policyholderWeight,
+                                   providerName, policyholderFirstname, policyholderLastname, smokingStatus);
 
-        //Print all the information.
-        System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
-        System.out.println("Provider Name: " + policy.getProviderName());
-        System.out.println("Policyholder's First Name: " + policy.getPolicyholderFirstname());
-        System.out.println("Policyholder's Last Name: " + policy.getPolicyholderLastname());
-        System.out.println("Policyholder's Age: " + policy.getPolicyholderAge());
-        System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
-        System.out.printf("Policyholder's Height: %.1f inches\n" , policy.getPolicyholderHeight());
-        System.out.printf("Policyholder's Weight: %.1f pounds\n" , policy.getPolicyholderWeight());
-        System.out.printf("Policyholder's BMI: %.2f\n" , policy.bmiCalcualtor());
-        System.out.printf("Policy Price: $%.2f\n" , policy.policyPriceCalculator());    
+        //put a blank line before the output
+        System.out.println();
+
+        policy.displayInformation();
+            
         
     }
 
